a8c5f8455bc6b8f0c6b4a85d063aa9e0baf23c3d
Muhammad Anis <muhammad.anis@veritrans.co.id>
2015-02-09 16:22:00 +0700

    Initial Commit

---
+++ VTMobileSDK/VTMobileSDK.h
@@ -0,0 +1,13 @@
+//
+//  VTMobileSDK.h
+//  VTMobileSDK
+//
+//  Created by Muhammad Anis on 2/9/15.
+//  Copyright (c) 2015 Veritrans Indonesia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VTMobileSDK : NSObject
+
+@end
